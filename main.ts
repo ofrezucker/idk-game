fe2e74c3f6244daf9f5fa253b9677d02403b5647
ofrezucker <112804727+ofrezucker@users.noreply.github.com>
2022-09-11 12:11:59 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+let the_activety = randint(1, 7)
+basic.forever(function () {
+    if (the_activety == 1) {
+    	
+    } else if (the_activety == 2) {
+    	
+    } else if (the_activety == 3) {
+    	
+    } else if (the_activety == 4) {
+    	
+    } else if (the_activety == 5) {
+    	
+    } else if (the_activety == 6) {
+    	
+    } else if (the_activety == 7) {
+    	
+    } else {
+    	
+    }
+})
